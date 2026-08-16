38cda367af4d79361a799e816bbd0eaff227e636
tozxart <fallag.rayen@gmail.com>
2026-08-16 18:31:22 +0100

    feat: update icon references in layout and add new icon.svg file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -7,6 +9,9 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   output: "export",
+  trailingSlash: true,
+  basePath: isProd ? "/Code-Circulation" : "",
+  assetPrefix: isProd ? "/Code-Circulation/" : "",
   images: {
     unoptimized: true,
   },
